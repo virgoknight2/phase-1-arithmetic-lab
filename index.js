036f848f2446d4ceb60aac36e94c3577e291d0d0
Lorelai Knutti <virgoknight2@gmail.com>
2022-04-05 09:21:51 -0400

    commit tree 6353190856aef38767025a618cff22c78bf2b225

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1=2
+const num2=31
+const num3=12
+const num4=3
+const multiply = num1*num2
+const random = Math.floor(Math.random()+1)
+const mod = num3/num4
+const max = Math.max(1,4,8,12,16,20)
